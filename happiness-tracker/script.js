703cac613785696d81e77c7127656d30b9d54119
Guillaume Dubois <g.dubois@mancheopenschool.fr>
2015-10-29 10:57:16 +0100

    Happiness - Ajout avatars w/ Bootstrap grid51 & Input date

--- happiness-tracker/script.js
+++ happiness-tracker/script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
 	$('#tabs').tabs();
+	$('#datepicker').datepicker();
+	$('#datepicker').datepicker($.datepicker.regional['fr']);
+	$('#datepicker').datepicker('option','dateFormat','d MM yy');
 });
