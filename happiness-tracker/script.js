c1996a503f6db9f5d3c7164f12873d585727f2bf
Guillaume Dubois <g.dubois@mancheopenschool.fr>
2015-11-16 15:28:51 +0100

    Happiness - Mood image

--- happiness-tracker/script.js
+++ happiness-tracker/script.js
@@ -17,10 +17,45 @@ $(document).ready(function(){
 		range: 'min', min: 1, max: 5, value: 3, slide: function(event, ui){
 			$('#happiness-life').val(ui.value);
 		}
-	});
+	}); 
 	$('#growth-code').val($('#slider-growth-code').slider('value'));
 	$('#happiness-code').val($('#slider-happiness-code').slider('value'));
 	$('#happiness-life').val($('#slider-happiness-life').slider('value'));
+	$('#slider-growth-code, #slider-happiness-code, #slider-happiness-life').on("slide", function(event, ui){
+		var somme = $('#slider-growth-code').slider('value')
+					+ $('#slider-happiness-code').slider('value')
+					+ $('#slider-happiness-life').slider('value');
+		if (somme < 5){
+			$('img.mood').attr( "src", function() {
+				return "images/smileys/1.jpg";
+			});
+		}
+		else if (somme < 7){
+			$('img.mood').attr( "src", function() {
+				return "images/smileys/2.jpg";
+			});
+		}
+		else if (somme < 9){
+			$('img.mood').attr( "src", function() {
+				return "images/smileys/3.jpg";
+			});
+		}
+		else if (somme < 12){
+			$('img.mood').attr( "src", function() {
+				return "images/smileys/4.jpg";
+			});
+		}
+		else if (somme < 14){
+			$('img.mood').attr( "src", function() {
+				return "images/smileys/5.jpg";
+			});
+		}
+		else{
+			$('img.mood').attr( "src", function() {
+				return "images/smileys/6.jpg";
+			});
+		}
+	});
 	$('#accordion').accordion({
 		collapsible: true, active: false, heightStyle: "content", animate: 100
 	});
